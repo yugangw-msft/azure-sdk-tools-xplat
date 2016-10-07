354270c876d796f2d23ebdcd8266fa9ad4473718
Andrew Mestas <andrew.mestas.dev@gmail.com>
2016-10-06 17:58:04 -0700

    Update suite.arm-cli-powerbiembedded-tests.nock.js

--- test/recordings/arm-cli-powerbiembedded-tests/suite.arm-cli-powerbiembedded-tests.nock.js
+++ test/recordings/arm-cli-powerbiembedded-tests/suite.arm-cli-powerbiembedded-tests.nock.js
@@ -30,4 +30,4 @@ exports.setEnvironment = function() {
 
 exports.scopes = [];
 
- exports.randomTestIdsGenerated = function() { return ['azureXplatCliTestResourceGroup1125'];};
+ exports.randomTestIdsGenerated = function() { return ['azureXplatCliTestResourceGroup5228'];};
