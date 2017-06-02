00aff3847cb0a3b3b64af8f1211e6081bb059ea0
Evgeny Agafonchikov <evgeny.agafonchikov@akvelon.com>
2017-06-02 14:27:52 +0300

    Uncomment group deletion in app-gw test

--- test/commands/arm/network/arm.network.application-gateway-tests.js
+++ test/commands/arm/network/arm.network.application-gateway-tests.js
@@ -149,9 +149,9 @@ describe('arm', function () {
     });
     after(function (done) {
       this.timeout(hour);
-      //networkUtil.deleteGroup(groupName, suite, function () {
+      networkUtil.deleteGroup(groupName, suite, function () {
         suite.teardownSuite(done);
-      //});
+      });
     });
     beforeEach(function (done) {
       suite.setupTest(done);
