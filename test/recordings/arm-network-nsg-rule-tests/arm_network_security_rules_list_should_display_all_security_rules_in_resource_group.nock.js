b5a3222bd4637de5131a2e36d4d122b6f6f18f95
Anton Evseev <anton.evseev@akvelon.com>
2017-06-05 10:19:11 +0300

    Updated NSG's autogenerated tests

--- test/recordings/arm-network-nsg-rule-tests/arm_network_security_rules_list_should_display_all_security_rules_in_resource_group.nock.js
+++ test/recordings/arm-network-nsg-rule-tests/arm_network_security_rules_list_should_display_all_security_rules_in_resource_group.nock.js
@@ -30,35 +30,35 @@ exports.scopes = [[function (nock) {
 var result = 
 nock('http://management.azure.com:443')
   .get('/subscriptions/2c224e7e-3ef5-431d-a57b-e71f4662e3a6/resourceGroups/xplat-test-rule/providers/Microsoft.Network/networkSecurityGroups/networkSecurityGroupName/securityRules?api-version=2017-03-01')
-  .reply(200, "{\r\n  \"value\": [\r\n    {\r\n      \"name\": \"securityRuleName\",\r\n      \"id\": \"/subscriptions/2c224e7e-3ef5-431d-a57b-e71f4662e3a6/resourceGroups/xplat-test-rule/providers/Microsoft.Network/networkSecurityGroups/networkSecurityGroupName/securityRules/securityRuleName\",\r\n      \"etag\": \"W/\\\"85a20f33-eb0a-4661-8087-8b86ed014d4d\\\"\",\r\n      \"properties\": {\r\n        \"provisioningState\": \"Succeeded\",\r\n        \"description\": \"setdesc\",\r\n        \"protocol\": \"Udp\",\r\n        \"sourcePortRange\": \"65535\",\r\n        \"destinationPortRange\": \"65042\",\r\n        \"sourceAddressPrefix\": \"10.0.0.0/8\",\r\n        \"destinationAddressPrefix\": \"11.0.0.0/8\",\r\n        \"access\": \"Deny\",\r\n        \"priority\": 1542,\r\n        \"direction\": \"Outbound\"\r\n      }\r\n    }\r\n  ]\r\n}", { 'cache-control': 'no-cache',
+  .reply(200, "{\r\n  \"value\": [\r\n    {\r\n      \"name\": \"securityRuleName\",\r\n      \"id\": \"/subscriptions/2c224e7e-3ef5-431d-a57b-e71f4662e3a6/resourceGroups/xplat-test-rule/providers/Microsoft.Network/networkSecurityGroups/networkSecurityGroupName/securityRules/securityRuleName\",\r\n      \"etag\": \"W/\\\"a25cb080-4420-4345-8d0f-7482f36a2b6c\\\"\",\r\n      \"properties\": {\r\n        \"provisioningState\": \"Succeeded\",\r\n        \"description\": \"setdesc\",\r\n        \"protocol\": \"Udp\",\r\n        \"sourcePortRange\": \"65535\",\r\n        \"destinationPortRange\": \"65042\",\r\n        \"sourceAddressPrefix\": \"10.0.0.0/8\",\r\n        \"destinationAddressPrefix\": \"11.0.0.0/8\",\r\n        \"access\": \"Deny\",\r\n        \"priority\": 1542,\r\n        \"direction\": \"Outbound\"\r\n      }\r\n    }\r\n  ]\r\n}", { 'cache-control': 'no-cache',
   pragma: 'no-cache',
   'content-length': '739',
   'content-type': 'application/json; charset=utf-8',
   expires: '-1',
-  'x-ms-request-id': '8b46bc6d-6c8d-4020-a228-3923c1b5a2e5',
+  'x-ms-request-id': '42ed21cf-2a1e-4e25-89d6-0a292f8e1863',
   'strict-transport-security': 'max-age=31536000; includeSubDomains',
-  server: 'Microsoft-HTTPAPI/2.0, Microsoft-HTTPAPI/2.0',
+  server: 'Microsoft-HTTPAPI/2.0',
   'x-ms-ratelimit-remaining-subscription-reads': '14996',
-  'x-ms-correlation-request-id': '5da439a3-9ae3-4695-9486-e7a5c9b852d7',
-  'x-ms-routing-request-id': 'WESTEUROPE:20170313T083829Z:5da439a3-9ae3-4695-9486-e7a5c9b852d7',
-  date: 'Mon, 13 Mar 2017 08:38:28 GMT',
+  'x-ms-correlation-request-id': 'c2adc334-6140-4063-8ff2-8d0bac03dd90',
+  'x-ms-routing-request-id': 'WESTEUROPE:20170605T071419Z:c2adc334-6140-4063-8ff2-8d0bac03dd90',
+  date: 'Mon, 05 Jun 2017 07:14:19 GMT',
   connection: 'close' });
  return result; },
 function (nock) { 
 var result = 
 nock('https://management.azure.com:443')
   .get('/subscriptions/2c224e7e-3ef5-431d-a57b-e71f4662e3a6/resourceGroups/xplat-test-rule/providers/Microsoft.Network/networkSecurityGroups/networkSecurityGroupName/securityRules?api-version=2017-03-01')
-  .reply(200, "{\r\n  \"value\": [\r\n    {\r\n      \"name\": \"securityRuleName\",\r\n      \"id\": \"/subscriptions/2c224e7e-3ef5-431d-a57b-e71f4662e3a6/resourceGroups/xplat-test-rule/providers/Microsoft.Network/networkSecurityGroups/networkSecurityGroupName/securityRules/securityRuleName\",\r\n      \"etag\": \"W/\\\"85a20f33-eb0a-4661-8087-8b86ed014d4d\\\"\",\r\n      \"properties\": {\r\n        \"provisioningState\": \"Succeeded\",\r\n        \"description\": \"setdesc\",\r\n        \"protocol\": \"Udp\",\r\n        \"sourcePortRange\": \"65535\",\r\n        \"destinationPortRange\": \"65042\",\r\n        \"sourceAddressPrefix\": \"10.0.0.0/8\",\r\n        \"destinationAddressPrefix\": \"11.0.0.0/8\",\r\n        \"access\": \"Deny\",\r\n        \"priority\": 1542,\r\n        \"direction\": \"Outbound\"\r\n      }\r\n    }\r\n  ]\r\n}", { 'cache-control': 'no-cache',
+  .reply(200, "{\r\n  \"value\": [\r\n    {\r\n      \"name\": \"securityRuleName\",\r\n      \"id\": \"/subscriptions/2c224e7e-3ef5-431d-a57b-e71f4662e3a6/resourceGroups/xplat-test-rule/providers/Microsoft.Network/networkSecurityGroups/networkSecurityGroupName/securityRules/securityRuleName\",\r\n      \"etag\": \"W/\\\"a25cb080-4420-4345-8d0f-7482f36a2b6c\\\"\",\r\n      \"properties\": {\r\n        \"provisioningState\": \"Succeeded\",\r\n        \"description\": \"setdesc\",\r\n        \"protocol\": \"Udp\",\r\n        \"sourcePortRange\": \"65535\",\r\n        \"destinationPortRange\": \"65042\",\r\n        \"sourceAddressPrefix\": \"10.0.0.0/8\",\r\n        \"destinationAddressPrefix\": \"11.0.0.0/8\",\r\n        \"access\": \"Deny\",\r\n        \"priority\": 1542,\r\n        \"direction\": \"Outbound\"\r\n      }\r\n    }\r\n  ]\r\n}", { 'cache-control': 'no-cache',
   pragma: 'no-cache',
   'content-length': '739',
   'content-type': 'application/json; charset=utf-8',
   expires: '-1',
-  'x-ms-request-id': '8b46bc6d-6c8d-4020-a228-3923c1b5a2e5',
+  'x-ms-request-id': '42ed21cf-2a1e-4e25-89d6-0a292f8e1863',
   'strict-transport-security': 'max-age=31536000; includeSubDomains',
-  server: 'Microsoft-HTTPAPI/2.0, Microsoft-HTTPAPI/2.0',
+  server: 'Microsoft-HTTPAPI/2.0',
   'x-ms-ratelimit-remaining-subscription-reads': '14996',
-  'x-ms-correlation-request-id': '5da439a3-9ae3-4695-9486-e7a5c9b852d7',
-  'x-ms-routing-request-id': 'WESTEUROPE:20170313T083829Z:5da439a3-9ae3-4695-9486-e7a5c9b852d7',
-  date: 'Mon, 13 Mar 2017 08:38:28 GMT',
+  'x-ms-correlation-request-id': 'c2adc334-6140-4063-8ff2-8d0bac03dd90',
+  'x-ms-routing-request-id': 'WESTEUROPE:20170605T071419Z:c2adc334-6140-4063-8ff2-8d0bac03dd90',
+  date: 'Mon, 05 Jun 2017 07:14:19 GMT',
   connection: 'close' });
  return result; }]];
